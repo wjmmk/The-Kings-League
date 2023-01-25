bf9087d7a792067d36afc8a4b83d630aeeb3a438
William Mosquera <wjmmk3@gmail.com>
2023-01-25 14:55:01 -0500

    added assets folder to contain static assets

--- api/index.js
+++ api/index.js
@@ -1,22 +1,21 @@
 import { Hono } from 'hono'
+import { serveStatic } from 'hono/serve-static'
 
 import leaderboard from '../db/leaderboard.json'
 import teams from '../db/teams.json'
 
 const app = new Hono()
 
-app.get('/', (ctx) => {
-  return ctx.json([
-    {
-      endpoint: '/leaderboard',
-      description: 'Returns the leaderboard statistics'
-    },
-    {
-      endpoint: '/teams',
-      description: 'Returns the teams statistics'
-    }
-  ])
-})
+app.get('/', (ctx) => ctx.json([
+  {
+    endpoint: '/leaderboard',
+    description: 'Returns the leaderboard statistics'
+  },
+  {
+    endpoint: '/teams',
+    description: 'Returns the teams statistics'
+  }
+]))
 
 app.get('/leaderboard', (ctx) => {
   return ctx.json(leaderboard)
@@ -26,15 +25,6 @@ app.get('/teams', (ctx) => {
   return ctx.json(teams)
 })
 
-// Esta es la forma antigua de hacer una peticion.
-/* export default {
-  async fetch (request, env, ctx) {
-    return new Response(JSON.stringify(leaderboard), {
-      headers: {
-        'content-type': 'application/json;charset=UTF-8'
-      }
-    })
-  }
-} */
+app.get('/static/*', serveStatic({ path: './assets' }))
 
 export default app
