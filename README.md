0ac14e42ccacd5e25a40a8b2fd0ffc1226e05554
William Mosquera <wjmmk3@gmail.com>
2023-01-31 08:07:17 -0500

    added assets folder to contain static assets Refactor

---
+++ README.md
@@ -0,0 +1,5 @@
+TEAMS: https://kingsleague.pro/wp-json/wp/v2/teams
+
+PLAYERS: https://kingsleague.pro/wp-json/wp/v2/playerteams
+
+GAMES: https://kingsleague.pro/wp-json/wp/v2/matches
--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 import { Hono } from 'hono'
-import { serveStatic } from 'hono/serve-static'
+import { serveStatic } from 'hono/serve-static.module'
 
 import leaderboard from '../db/leaderboard.json'
 import teams from '../db/teams.json'
@@ -14,6 +14,10 @@ app.get('/', (ctx) => ctx.json([
   {
     endpoint: '/teams',
     description: 'Returns the teams statistics'
+  },
+  {
+    endpoint: '/static/logos/id:',
+    description: 'Returns the teams statistics'
   }
 ]))
 
@@ -25,6 +29,6 @@ app.get('/teams', (ctx) => {
   return ctx.json(teams)
 })
 
-app.get('/static/*', serveStatic({ path: './assets' }))
+app.get('/static/*', serveStatic({ root: './' }))
 
 export default app
